7b5dcb3ee9d52ad5c9c7d11b7572fabbdb9fe8f4
ANGELA-NYUTU <angie.nimo254@gmail.com>
2026-08-02 02:25:21 +0300

    final

--- main.py
+++ main.py
@@ -1,39 +1,69 @@
 # STEP 1A
-# Import SQL Library and Pandas
+# Import SQLITE and pandas
+import sqlite3
+import pandas as pd
 
 # STEP 1B
 # Connect to the database
-conn = None
+conn = sqlite3.connect("data.sqlite")
+# Add code below and run file to see data from employees table
+
+employee_data = pd.read_sql("SELECT * FROM employees", conn)
+print("---------------------Employee Data---------------------")
+print(employee_data)
+print("-------------------End Employee Data-------------------")
 
 
 # STEP 2
-# Replace None with your code
-df_first_five = None
+df_first_five = employee_data[['employeeNumber', 'lastName']].head(5)
+
+print(df_first_five)
 
 # STEP 3
 # Replace None with your code
-df_five_reverse = None
-
+df_five_reverse = employee_data[['lastName','employeeNumber']].tail(5)
+print(df_five_reverse)
 # STEP 4
 # Replace None with your code
-df_alias = None
+df_alias = employee_data.rename(columns={"employeeNumber": "ID"})[["lastName","ID"]].tail(5)
+print(df_alias)
 
 # STEP 5
-# Replace None with your code
-df_executive = None
+df_executive = employee_data.copy()
+df_executive["role"] = "Not Executive"
+df_executive.loc[
+    df_executive["jobTitle"].isin(["President", "VP Sales", "VP Marketing"]),
+    "role"
+] = "Executive"
+print(df_executive)
 
 # STEP 6
 # Replace None with your code
-df_name_length = None
-
+df_name_length = employee_data["lastName"].str.len().to_frame(name="name_length")
+print(df_name_length)
 # STEP 7
 # Replace None with your code
-df_short_title = None
+df_short_title = employee_data["jobTitle"].str[:2].to_frame(name="short_title")
+print(df_short_title)
 
 # STEP 8
 # Replace None with your code
-sum_total_price = None
+sum_total_price = pd.read_sql("""
+SELECT ROUND(priceEach * quantityOrdered) AS total_price
+FROM orderDetails;
+""", conn)["total_price"].sum()
+print(sum_total_price)
 
 # STEP 9
 # Replace None with your code
-df_day_month_year = None
+df_day_month_year = pd.read_sql("""
+SELECT
+    orderDate,
+    strftime('%d', orderDate) AS day,
+    strftime('%m', orderDate) AS month,
+    strftime('%Y', orderDate) AS year
+FROM orders;
+""", conn)
+
+print(df_day_month_year)
+conn.close()
